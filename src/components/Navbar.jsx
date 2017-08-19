22f9febe4ab532a9585167b8b74b25d6ed812f1e
Joe <joe@joedaniels.co.uk>
2017-08-19 01:36:44 +0100

    code clean up
    moving from 2 to 4 spaces for indendation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,13 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 export default class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <div className="container">
-            <span className="navbar-brand">RedFruit Timer</span>
-
-            <span className="navbar-text">Created by <a href="https://joedaniels.co.uk/">Joe Daniels</a></span>
-        </div>
-      </nav>
-    )
-  }
+    render() {
+        return (
+            <nav className="navbar navbar-light bg-light">
+                <div className="container">
+                    <span className="navbar-brand">RedFruit Timer</span>
+                </div>
+            </nav>
+        )
+    }
 }
---
+++ src/components/Timer.jsx
@@ -0,0 +1,96 @@
+import React, {Component} from 'react'
+
+export default class Timer extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            clock: this.props.pomodoroLength,
+            timerLength: this.props.pomodoroLength,
+            breakNumber: 1
+        }
+
+        this.tick = this.tick.bind(this)
+        this.countdown = this.countdown.bind(this)
+        this.pause = this.pause.bind(this)
+        this.reset = this.reset.bind(this)
+    }
+
+    setBreak(breakType, breakNumber) {
+        return this.setState({
+            clock: breakType,
+            timerLength: breakType,
+            breakNumber: breakNumber
+        })
+    }
+
+    tick() {
+        if (this.state.clock === 0) {
+            this.pause()
+
+            if (this.state.timerLength === (this.props.shortBreak || this.props.longBreak)) {
+                this.setState({
+                    clock: this.props.pomodoroLength,
+                    timerLength: this.props.pomodoroLength
+                })
+            } else {
+                if (this.state.breakNumber > 3) {
+                    this.setBreak(this.props.longBreak, 1)
+                } else {
+                    this.setBreak(this.props.shortBreak, this.state.breakNumber + 1)
+                }
+            }
+        } else {
+            this.setState({ clock: this.state.clock - 1 })
+        }
+    }
+
+    countdown() {
+        if (!this.interval) {
+            this.interval = setInterval(this.tick, 1000)
+        }
+    }
+
+    pause() {
+        clearInterval(this.interval)
+        this.interval = false
+    }
+
+    reset() {
+        if (this.interval) {
+            this.pause()
+            this.setState({ clock: this.state.timerLength })
+        } else {
+            this.setState({ clock: this.state.timerLength })
+        }
+    }
+
+    prettifyClock(timeLeft) {
+        let m = Math.floor(timeLeft / 60)
+        let s = Math.floor(timeLeft % 60)
+
+        return this.addZero(m) + ":" + this.addZero(s)
+    }
+
+    addZero(n) {
+        return (n < 10 ? "0" : "") + n
+    }
+
+
+    static defaultProps = {
+        pomodoroLength: 1500,
+        shortBreak: 300,
+        longBreak: 900
+    }
+
+    render() {
+        return (
+            <section>
+                <span className="timer">{this.prettifyClock(this.state.clock)}</span>
+                <button className="btn btn-primary btn-lg" onClick={this.countdown}>Start</button>
+                <button className="btn btn-secondary btn-sm" onClick={this.pause}>Pause</button>
+                <button className="btn btn-dark btn-sm" onClick={this.reset}>Reset</button>
+            </section>
+        )
+    }
+}
